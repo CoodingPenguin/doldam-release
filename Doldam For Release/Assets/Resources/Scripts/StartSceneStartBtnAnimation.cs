7a8db6896ee87bf2aef50b6f7a15618f5bdd7b7c
박성민 <moktak072@gmail.com>
2019-07-17 14:25:09 +0900

    * Material 폴더에 에 배경 Blur 효과를 주는 BlurMateria을 추가해두었습니다. 필요하면 적용시켜서 쓰면 됩니다. Radius로 흐림정도를 고를 수 있어요
    한 것
    - BlurMaterial 추가
    - StartScene Canvas 구성
    - GameScene Canvas 구성
    
    할 것
    - CreditScene Canvas 구성
    
    - 각 Scene의 Canvas에 적당한 이미지 삽입
    - 버튼마다 적절한 이미지 삽입
    
    - Scene을 연결하는 코드 작성
    	- StartScene
    	- GameScene
    	- CreditScene

---
+++ Doldam For Release/Assets/Resources/Scripts/StartSceneStartBtnAnimation.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartSceneStartBtnAnimation : MonoBehaviour
+{
+    public Color32 colors;
+    private float speed = 1f;
+    private float transparentRatio = 0f;
+    private bool transparentRatioIsIncreasing = true;
+
+    // value를 입력해줄 때 증가상태이면 계속 증가, 감소상태이면 계속 감소하도록 하는 역할을 Get Set으로 구현함.
+    // ratio * 255 값으로 투명도를 적용한다.
+    public float TransparentRatio
+    {
+        get { return transparentRatio; }
+
+        // transparentRatio는 0~1의 값을 가진다
+        set
+        {
+            if (transparentRatioIsIncreasing)
+            {
+                transparentRatio += value;
+                if(transparentRatio >= 1)
+                {
+                    transparentRatio = 1f;
+                    transparentRatioIsIncreasing = false;
+                }
+            }
+
+            else
+            {
+                transparentRatio -= value;
+                if (transparentRatio <= 0)
+                {
+                    transparentRatio = 0f;
+                    transparentRatioIsIncreasing = true;
+                }
+            }
+        }
+    }
+
+    private void Start()
+    {
+        colors = this.gameObject.GetComponent<Text>().color;
+    }
+
+    private void Update()
+    {
+        TransparentRatio = speed * Time.deltaTime;
+        this.gameObject.GetComponent<Text>().color = new Color32(colors.r, colors.g, colors.b, (byte)(TransparentRatio * 255));
+    }
+}
